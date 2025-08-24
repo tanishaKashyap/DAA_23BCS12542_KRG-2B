78c01c9c7e706ea2c00af493b90d438909f3b2ca
tanishaKashyap <tanisha.pankajj@gmail.com>
2025-08-24 18:17:23 +0530

    Update EXP-2.cpp

--- EXP-2/EXP-2.cpp
+++ EXP-2/EXP-2.cpp
@@ -1,34 +1,19 @@
-#include <iostream>
-using namespace std;
-
-double power(double x, int y) {
-    if (y == 0)
-        return 1; 
-    
-    double temp = power(x, y / 2);
-    
- 
-    if (y % 2 == 0) {
-        return temp * temp;
-    } 
-
-    else {
-        if (y > 0)
-            return x * temp * temp; 
-        else
-            return (temp * temp) / x; 
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long exp = n;
+        if (exp < 0) {
+            x = 1 / x;
+            exp = -exp;
+        }
+        double ans = 1.0;
+        while (exp > 0) {
+            if (exp % 2 == 1) {
+                ans *= x;
+            }
+            x *= x;
+            exp /= 2;
+        }
+        return ans;
     }
-}
-
-int main() {
-    double base;
-    int exponent;
-    
-    cout << "Enter base: ";
-    cin >> base;
-    cout << "Enter exponent: ";
-    cin >> exponent;
-    
-    cout << base << "^" << exponent << " = " << power(base, exponent) << endl;
-    return 0;
-}
+};
